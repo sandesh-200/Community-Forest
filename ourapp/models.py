15f1fabff16d8749134f96f5aaad2385e70041d6
Sandesh <dsandesh263@gmail.com>
2024-08-13 22:09:22 +0545

    Sandesh Cloning

--- ourapp/models.py
+++ ourapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MinValueValidator, MaxValueValidator
 #Following are models
+#By sandesh
 class AllUser(models.Model):
     first_name = models.CharField(max_length=200,blank=False,null=False)
     middle_name = models.CharField(max_length=200,blank=True,null=True)
